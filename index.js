d6d49d9a26856b9b2b00d628aad92d39fcdcf756
Lyudmyla26 <l.stesko@ukr.net>
2024-03-18 09:28:09 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const hero = document.querySelector('.hero')
 const avatar = document.querySelector('.avatar-container')
-const size = hero.getBoundingClientRect();
+function mobile() {
+    return window.innerWidth < 768;
+    
+}
+if (mobile()) {
+    const size = hero.getBoundingClientRect();
 const height = size.height; 
 avatar.style.marginBottom = height + 10 + 'px';
+}
+
 
 function isElementVisible(el) {
     const rect = el.getBoundingClientRect();
@@ -21,7 +28,14 @@ function animateSections() {
             section.classList.remove('active');
         }
     });
- 
+  const sections2 = document.querySelectorAll('.animate-sections');
+   sections2.forEach(function (section) { // Виправлено назву змінної
+        if (isElementVisible(section)) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    });
 }
 
 window.addEventListener('scroll', function() {
